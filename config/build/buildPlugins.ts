546e70d03b4486ea90333c3943aa800680cd2130
Viktor Aleksandrov <viqto0r@gmail.com>
2024-04-13 16:26:23 +0300

    configure githu actions

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -8,7 +8,7 @@ export const buildPlugins = ({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] => {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -20,9 +20,16 @@ export const buildPlugins = ({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
   ]
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin())
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      })
+    )
+  }
+
+  return plugins
 }
