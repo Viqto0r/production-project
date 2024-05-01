c0aefb73352d9372f01722d53313157c9b6e95dc
Viktor Aleksandrov <viqto0r@gmail.com>
2024-05-01 18:38:33 +0300

    add precommit and update screenshots

--- src/entitites/Counter/model/slice/counterSlice.ts
+++ src/entitites/Counter/model/slice/counterSlice.ts
@@ -10,11 +10,9 @@ const counterSlice = createSlice({
   initialState,
   reducers: {
     increment: (state) => {
-      console.log('increment')
       state.value += 1
     },
     decrement: (state) => {
-      console.log('decrement')
       state.value -= 1
     },
   },
