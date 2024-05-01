4e7707b61a81f882d257f0be500db88caf781383
Viktor Aleksandrov <viqto0r@gmail.com>
2024-05-01 18:47:31 +0300

    edit pre-commit settings and remove Counter

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,7 @@
-import { Counter } from 'entitites/Counter'
 import { useTranslation } from 'react-i18next'
 
 export default function MainPage() {
   const { t } = useTranslation()
 
-  return (
-    <div>
-      <div>{t('главная страница')}</div>
-      <Counter />
-    </div>
-  )
+  return <div>{t('главная страница')}</div>
 }
