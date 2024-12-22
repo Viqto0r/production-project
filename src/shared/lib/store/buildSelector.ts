94da8a2e1806fcf4b36230dd42e6e81038d3a86b
Viqtor <viqto0r@gmail.com>
2024-12-22 16:15:10 +0300

    add args for buildSelector

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,11 +1,18 @@
 import { IStateSchema } from '@/app/providers/StoreProvider'
 import { useSelector } from 'react-redux'
 
-type TSelector<T> = (state: IStateSchema) => T
-type TResult<T> = [() => T, TSelector<T>]
+type TSelector<T, Args extends unknown[]> = (
+  state: IStateSchema,
+  ...args: Args
+) => T
+type THook<T, Args extends unknown[]> = (...args: Args) => T
+type TResult<T, Args extends unknown[]> = [THook<T, Args>, TSelector<T, Args>]
 
-export const buildSelector = <T>(selector: TSelector<T>): TResult<T> => {
-  const useSelectorHook = () => useSelector(selector)
+export const buildSelector = <T, Args extends unknown[]>(
+  selector: TSelector<T, Args>
+): TResult<T, Args> => {
+  const useSelectorHook: THook<T, Args> = (...args: Args) =>
+    useSelector((state: IStateSchema) => selector(state, ...args))
 
   return [useSelectorHook, selector]
 }
